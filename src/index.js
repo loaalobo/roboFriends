1117d36ee00e12ff7bdd70fc6aa1d706909238d6
Lorena Lobo <74051141+loaalobo@users.noreply.github.com>
2022-06-09 09:58:26 -0300

    increment project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-ReactDOM.render(<App />,  document.getElementById('root'));
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 
 // If you want to start measuring performance in your app, pass a function
